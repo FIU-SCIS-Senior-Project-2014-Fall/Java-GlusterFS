7cb4493f01ce7c51522338f2ea666bc39540e90f
Maylem Gonzalez <mgonz108@fiu.edu>
2014-11-16 15:51:28 -0500

    Modified implementation of move() to conform to Java 7 specs.

--- glusterfs-java-filesystem/glusterfs-java-filesystem/src/main/java/com/peircean/glusterfs/GlusterFileSystemProvider.java
+++ glusterfs-java-filesystem/glusterfs-java-filesystem/src/main/java/com/peircean/glusterfs/GlusterFileSystemProvider.java
@@ -232,6 +232,10 @@ public class GlusterFileSystemProvider extends FileSystemProvider {
 
     @Override
     public void move(Path path, Path path2, CopyOption... copyOptions) throws IOException {
+        if (path.equals(path2)) {
+            return;
+        }
+
         boolean overwrite = false;
         for (CopyOption co : copyOptions) {
             if (StandardCopyOption.ATOMIC_MOVE.equals(co)) {
@@ -241,15 +245,16 @@ public class GlusterFileSystemProvider extends FileSystemProvider {
                 overwrite = true;
             }
         }
-        boolean exists = Files.exists(path2);
-        if (!overwrite && exists) {
+
+        if (!overwrite && Files.exists(path2)) {
             throw new FileAlreadyExistsException("Target " + path2 + " exists and REPLACE_EXISTING not specified");
-        } else if (Files.isDirectory(path2) && !directoryIsEmpty(path2)) {
+        }
+        if (Files.isDirectory(path2) && !directoryIsEmpty(path2)) {
             throw new DirectoryNotEmptyException("Target not empty: " + path2);
         }
         FileSystem fileSystem = path.getFileSystem();
         if (!fileSystem.equals(path2.getFileSystem())) {
-            throw new UnsupportedOperationException("Can not move file to a different GlusterFS volume");
+            throw new UnsupportedOperationException("Can not move file to a different file system");
         }
         GLFS.glfs_rename(((GlusterFileSystem) fileSystem).getVolptr(), ((GlusterPath) path).getString(), ((GlusterPath) path2).getString());
     }
